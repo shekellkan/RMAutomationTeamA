941dc2c50da1333d29932842565e2ad90c626948
JeanCarloRM <alejandrojeancarlo@gmail.com>
2015-12-03 16:40:23 -0400

    base structure of the project

---
+++ src/test/java/runner/RunCukesTest.java
@@ -0,0 +1,38 @@
+package runner;
+
+import Framework.DriverManager;
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+import org.apache.log4j.Logger;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import ui.PageTransporter;
+import ui.common.CommonMethods;
+
+@CucumberOptions(
+        plugin = {"pretty", "html:target/cucumber", "json:target/cucumber.json"},
+        glue={"steps"},
+        features = {"src/test/resources/features"},
+//        features = {"src/test/resources/features/filebroker/ingest.feature"},
+        monochrome = true)
+public class RunCukesTest  extends AbstractTestNGCucumberTests {
+    final static Logger logger = Logger.getLogger(RunCukesTest.class);
+    @BeforeTest
+    public void beforeTest()
+    {
+        logger.info("Test Start!!!!!");
+        System.out.println("Before all the execution");
+    }
+
+    @AfterTest
+    public void afterExecution() {
+
+        try {
+            DriverManager.getInstance().quit();
+        } catch (Exception e) {
+            logger.error("Unable to quit the driver", e);
+        }
+        logger.info("End test!!!!!");
+
+    }
+}
---
+++ src/test/java/steps/hooks/GlobalHooks.java
@@ -0,0 +1,27 @@
+package steps.hooks;
+
+import Framework.DriverManager;
+import cucumber.api.Scenario;
+import cucumber.api.java.After;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: jeancarlorodriguez
+ * Date: 11/16/15
+ * Time: 8:07 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class GlobalHooks {
+    WebDriver webDriver = DriverManager.getInstance().getWebDriver();
+    @After
+    public void ScreenShot(Scenario scenario) {
+        if (scenario.isFailed()) {
+            // Take a screenshot...
+            final byte[] screenShot = ((TakesScreenshot) webDriver).getScreenshotAs(OutputType.BYTES);
+            scenario.embed(screenShot, "image/png"); // ... and embed it in the report.
+        }
+    }
+}
