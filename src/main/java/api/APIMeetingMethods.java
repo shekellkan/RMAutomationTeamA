57ab9c0e3faff1dca0dc87799619aeb02bc49509
MiguelTerceros <shekellkan07@hotmail.com>
2015-12-14 20:08:17 -0400

    changes in ExternalVariablesManager, environments and modify the APIManager

--- src/main/java/api/APIMeetingMethods.java
+++ src/main/java/api/APIMeetingMethods.java
@@ -24,56 +24,93 @@ public class APIMeetingMethods {
     String deleteMeetingEndPoint;
     String meetingEndPoint;
 
+    /**
+     * construct of APIMeetingMethods
+     */
     public APIMeetingMethods(){
         apiManager = new APIManager();
         mongoDBManager = new MongoDBManager();
     }
 
+    /**
+     * this method delete a meeting
+     * @param nameRoom
+     * @param nameMeeting
+     */
     public void deleteMeeting(String nameRoom, String nameMeeting){
-        String userName = ExternalVariablesManager.getInstance().getExchangeUserName();
-        String userPassword = ExternalVariablesManager.getInstance().getExchangeUserPassword();
+        String userAuthentication = ExternalVariablesManager.getInstance().getAuthenticationExchange();
 
         deleteMeetingEndPoint = buildEndPoint(nameRoom, nameMeeting);
-        apiManager.deleteBasic(deleteMeetingEndPoint+"/"+getMeetingId(nameMeeting), userName, userPassword);
+        apiManager.deleteBasic(deleteMeetingEndPoint, userAuthentication);
     }
 
+    /**
+     * this method obtains a value of the meeting json
+     * @param nameMeeting
+     * @param value
+     * @param nameRoom
+     * @return
+     */
     public String getMeetingValues(String nameMeeting, String value, String nameRoom) {
         meetingEndPoint = buildEndPoint(nameRoom, nameMeeting);
-        JSONObject jsonObject = apiManager.getJson(meetingEndPoint, getMeetingId(nameMeeting));
+        JSONObject jsonObject = apiManager.getJson(meetingEndPoint);
         return jsonObject.getString(value);
     }
 
+    /**
+     * obtains the serviceId
+     * @return serviceId
+     */
     public String getServiceId(){
         return mongoDBManager.getId("services", "type", "exchange");
     }
 
+    /**
+     * obtains the roomId
+     * @param nameRoom
+     * @return roomId
+     */
     public String getRoomId(String nameRoom){
         return mongoDBManager.getId("rooms", "displayName", nameRoom);
     }
 
+    /**
+     * obtains the meetingId
+     * @param nameMeeting
+     * @return meetingId
+     */
     public String getMeetingId(String nameMeeting){
         return mongoDBManager.getId("meetings", "title", nameMeeting);
     }
 
+    /**
+     * this method construct the path for a meeting in a room specific
+     * @param nameRoom
+     * @param nameMeeting
+     * @return
+     */
     public String buildEndPoint(String nameRoom, String nameMeeting){
         serviceId = getServiceId();
         roomId = getRoomId(nameRoom);
         meetingId = getMeetingId(nameMeeting);
 
-        return "/services/"+serviceId+"/rooms/"+roomId+"/meetings";
-        //return "/services/"+serviceId+"/rooms/"+roomId+"/meetings/"+meetingId;
+        return "/services/"+serviceId+"/rooms/"+roomId+"/meetings/"+meetingId;
     }
 
+    /**
+     * this method create a meeting
+     * @param meetingEntity
+     * @param roomName
+     */
     public void postMeeting(MeetingEntity meetingEntity, String roomName){
-        String userName = ExternalVariablesManager.getInstance().getExchangeUserName();
-        String userPassword = ExternalVariablesManager.getInstance().getExchangeUserPassword();
+        String userAuthentication = ExternalVariablesManager.getInstance().getAuthenticationExchange();
 
         SimpleDateFormat current= new SimpleDateFormat("yyyy-MM-dd");
         Date date = new Date();
         String currentDate = current.format(date);
 
         String deleteMeetingEndPoint = buildEndPoint(roomName, meetingEntity.getSubject());
-        given().auth().basic(userName,userPassword).
+        given().header("Authentication", "Basic "+userAuthentication).
                 parameters("organizer", meetingEntity.getOrganizer(),
                             "title", meetingEntity.getSubject(),
                             "start", currentDate+"T"+meetingEntity.getFrom()+":00.000Z",
